e49594a34b62270b0f2c125d3bc0ca34130f03af
Rick <dirklo@gmail.com>
2020-12-25 12:21:53 +0100

    commit tree 6c7286ea6ba9f54ddb032c9d3171c1d7a16b1790

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,17 @@
+class Course
+    attr_accessor :title, :schedule, :description
 
+    @@all = []
 
+    def self.reset_all
+        @@all = []
+    end
+
+    def self.all
+        @@all
+    end
+
+    def initialize
+        self.class.all << self
+    end
+end
